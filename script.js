6c34af051e1a48f0eff2d5a32f222b333488fab1
Rene <renesuar99@gmail.com>
2022-07-24 16:46:54 -0400

    error message now showing when password are not the same and hides when they are

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 
-
+let errorText = document.getElementById("errorText");
 let inputs = document.querySelectorAll("input");
 let password1 = document.getElementById('password');
 let passwordConfirm = document.getElementById('confirmPassword');
 const signupButton = document.getElementsByClassName('button2');
-console.log(password1);
+
 
 
 
 
 
 signupButton[0].addEventListener ('click', function (e){
-    // password1 = document.getElementById('password').value
-    // passwordConfirm = document.getElementById('confirmPassword').value
-    console.log(password1);
     if (passwordConfirm.value === password1.value) {
       
         for (i = 0; i < 6; i++)
         inputs[i].value = "";
         password1.classList.remove("error");
         passwordConfirm.classList.remove("error");
+        errorText.style.display = "none";
 
     }
 
@@ -27,6 +25,7 @@ signupButton[0].addEventListener ('click', function (e){
         password1.classList.add("error");
         passwordConfirm.classList.add("error");
         
+        errorText.style.display = "block";
 
 
     }
