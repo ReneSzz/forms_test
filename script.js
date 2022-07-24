58413a5d68c04bade6cc872ff23aa18b1d7da0df
Rene <renesuar99@gmail.com>
2022-07-24 16:34:32 -0400

    red error border and styling

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let password1 = document.getElementById('password').value;
-let passwordConfirm = document.getElementById('confirmPassword').value;
+
+
+let inputs = document.querySelectorAll("input");
+let password1 = document.getElementById('password');
+let passwordConfirm = document.getElementById('confirmPassword');
 const signupButton = document.getElementsByClassName('button2');
 console.log(password1);
 
+
+
+
+
 signupButton[0].addEventListener ('click', function (e){
-    password1 = document.getElementById('password').value
-    passwordConfirm = document.getElementById('confirmPassword').value
-    if (password1 !== passwordConfirm)
-    {
+    // password1 = document.getElementById('password').value
+    // passwordConfirm = document.getElementById('confirmPassword').value
+    console.log(password1);
+    if (passwordConfirm.value === password1.value) {
+      
+        for (i = 0; i < 6; i++)
+        inputs[i].value = "";
+        password1.classList.remove("error");
+        passwordConfirm.classList.remove("error");
+
+    }
+
+    if (passwordConfirm.value !== password1.value){ 
+        password1.classList.add("error");
+        passwordConfirm.classList.add("error");
         
+
+
     }
 });
